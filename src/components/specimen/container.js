6bfc0b23ffa7239cfa25adeb56f800db0e173012
Jesse Kuntz <jkuntz@groupon.com>
2020-07-13 22:29:36 -0500

    feat: filter for specimen

--- src/components/specimen/container.js
+++ src/components/specimen/container.js
@@ -1,12 +1,37 @@
+import {Fragment} from 'preact';
+
 import Specimen from './index';
 import Specimens from '../../assets/index';
 
 import style from './style';
 
+function filter() {
+  let input, filter, container, item, p, i, txtValue;
+  input = document.getElementById('specimenFilter');
+  filter = input.value.toUpperCase();
+  container = document.querySelector('.specimen-container');
+  item = container.getElementsByTagName('div');
+
+  for (i = 0; i < item.length; i++) {
+    p = item[i].getElementsByTagName('p')[0];
+    txtValue = p.textContent || p.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      item[i].style.display = "";
+    } else {
+      item[i].style.display = "none";
+    }
+  }
+}
+
 export default function SpecimenContainer({type}) {
+  const placeholder = `Search for a ${type}!`;
+
   return (
-    <div class={style.container}>
-      {Specimens[type].map(specimen => <Specimen url={`../../assets/${type}/png/${specimen.img}.png`} name={specimen.name} key={specimen.name} />)}
-    </div>
+    <Fragment>
+      <input type="text" id="specimenFilter" placeholder={placeholder} onKeyUp={filter} />
+      <div class={`${style.container} specimen-container`}>
+        {Specimens[type].map(specimen => <Specimen url={`../../assets/${type}/png/${specimen.img}.png`} name={specimen.name} key={specimen.name} />)}
+      </div>
+    </Fragment>
   );
 }
