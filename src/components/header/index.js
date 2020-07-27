1331e5095fff8f3c64ae8db1e20fbd81e35b931c
Jesse Kuntz <jkuntz@groupon.com>
2020-07-27 09:59:07 -0500

    feat: update to use universal emojies

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,12 +9,15 @@ const Header = () => (
 	<Fragment>
 		<header class={`${style['header-primary']} ${style.header}`}>
 			<div class={style.content}>
-				<Link activeClassName={style.active} href="/"><h1>AC Tracker ✅</h1></Link>
+				<Link activeClassName={style.active} href="/">
+					<h1>AC Tracker</h1>
+					<img class={style.check} src="../../assets/menu/check.svg" />
+				</Link>
 
 				<nav>
-					<Link activeClassName={style.active} href="/tracker/fish">🐡</Link>
-					<Link activeClassName={style.active} href="/tracker/bug">🦋</Link>
-					<Link activeClassName={style.active} href="/tracker/sea-creature">🤿</Link>
+					<Link activeClassName={style.active} href="/tracker/fish"><img src="../../assets/menu/fish.svg" /></Link>
+					<Link activeClassName={style.active} href="/tracker/bug"><img src="../../assets/menu/bug.svg" /></Link>
+					<Link activeClassName={style.active} href="/tracker/sea-creature"><img src="../../assets/menu/scuba.svg" /></Link>
 				</nav>
 			</div>
 		</header>
