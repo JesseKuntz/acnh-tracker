1e5ffd06be57ba90f1f2e7a2b5f1482c05d7618f
Jesse Kuntz <jkuntz@groupon.com>
2020-08-06 08:30:57 -0500

    feat: add styles and text to menu

---
+++ src/components/header/header-link.js
@@ -0,0 +1,19 @@
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
+
+import style from './style.css';
+
+function processType(type) {
+	return type.replace('-', ' ');
+}
+
+function HeaderLink({type, clickHandler}) {
+  return (<Link activeClassName={style.active} href={`/tracker/${type}`} onClick={clickHandler}>
+    <div class={`${style.content} ${style['menu-link-wrapper']}`}>
+      <div class={style['menu-text']}>{processType(type)}</div>
+      <img class={style.icon} src={`../../assets/menu/${type}.svg`} />
+    </div>
+  </Link>);
+}
+
+export default HeaderLink;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,10 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 
-import LoginInfo from '../login/info'
+import LoginInfo from '../login/info';
+import HeaderLink from './header-link';
 
 import style from './style.css';
-
 
 function getMenuClass(open) {
   return open ? style.show : style.hide;
@@ -32,7 +32,7 @@ class Header extends Component {
 			<div class={style['header-container']}>
 				<header class={`${style['header-primary']} ${style.header}`}>
 					<div class={style.content}>
-						<Link activeClassName={style.active} href="/">
+						<Link activeClassName={style.active} href="/" onClick={() => this.setState({isMenuOpen: false})}>
 							<h1>AC Tracker</h1>
 							<img class={style.check} src="../../assets/menu/check.svg" />
 						</Link>
@@ -48,16 +48,9 @@ class Header extends Component {
 				</header>
 				<header class={`${style['header-menu']} ${style.header}`}>
 					<nav class={getMenuClass(this.state.isMenuOpen)}>
-						{/* definitely have an opportunity for some DRY here */}
-						<Link activeClassName={style.active} href="/tracker/fish" onClick={() => this.clickHandler()}>
-							<img class={`${style.icon} ${style.content}`} src="../../assets/menu/fish.svg" />
-						</Link>
-						<Link activeClassName={style.active} href="/tracker/bug" onClick={() => this.clickHandler()}>
-							<img class={`${style.icon} ${style.content}`} src="../../assets/menu/bug.svg" />
-						</Link>
-						<Link activeClassName={style.active} href="/tracker/sea-creature" onClick={() => this.clickHandler()}>
-							<img class={`${style.icon} ${style.content}`} src="../../assets/menu/scuba.svg" />
-						</Link>
+						<HeaderLink type={'fish'} clickHandler={() => this.clickHandler()} />
+						<HeaderLink type={'bug'} clickHandler={() => this.clickHandler()} />
+						<HeaderLink type={'sea-creature'} clickHandler={() => this.clickHandler()} />
 					</nav>
 				</header>
 				<header class={`${style['header-secondary']} ${style.header}`}>
