1d3ad8937f58b4368d63d2e414ea9e06b4826397
Jesse Kuntz <jkuntz@groupon.com>
2020-09-08 20:38:47 -0500

    feat: add rest of files for hero image

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,6 +5,14 @@ import style from './style';
 const Home = () => (
   <div class={style.home}>
     <h1>Welcome to the Animal Crossing Specimen Tracker!</h1>
+    <picture>
+      <source srcset="../../assets/icons/hero-image.webp" type="image/webp" />
+      <source srcset="../../assets/icons/hero-image.png" type="image/png" />
+      <img
+        class={style['hero-image']}
+        src="../../assets/icons/hero-image.png"
+      />
+    </picture>
     <p>
       Each tab in the navigation will keep track of a different type of
       specimen. Once the functionality is built out, you will be able set alerts
